8472788801a561eae8040f393d43435fc5d16924
AHD-02 <a.alziq2002@gmail.com>
2022-04-06 15:18:53 +0300

    updates

--- makan-website/src/makanComponents/bannerPage.js
+++ makan-website/src/makanComponents/bannerPage.js
@@ -5,7 +5,7 @@ const Banner = () =>
     
     return(
      <div>
-     <img src="https://adminassets.devops.arabiaweather.com/sites/default/files/field/image/farm-chalet.jpg" width={300} height={300} />
+     <img src = "https://adminassets.devops.arabiaweather.com/sites/default/files/field/image/farm-chalet.jpg" width={300} height={300} />
      <img 
      src="https://adminassets.devops.arabiaweather.com/sites/default/files/field/image/farm-chalet.jpg"
      width={300} height={300} 
