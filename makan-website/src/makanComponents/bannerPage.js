10e9af481990a2cfc9196cc105c2d695a3c9d907
AHD-02 <a.alziq2002@gmail.com>
2022-04-06 14:17:10 +0300

    updates

--- makan-website/src/makanComponents/bannerPage.js
+++ makan-website/src/makanComponents/bannerPage.js
@@ -7,7 +7,6 @@ const Banner = () =>
      <div>
      <h3 className = "text-blue-600 hover:text-green-600 text-sky-400/50">Ahmad</h3>
      <nav>Alziq</nav>
-     <p>My Name is Ahmad</p>
      </div>
     )
 }
