7218d7d1029b8f81364eaa02ac1d995dd6d6732e
AHD-02 <a.alziq2002@gmail.com>
2022-04-11 14:30:53 +0300

    updates

--- makan-website/src/App.js
+++ makan-website/src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 import Banner from './makanComponents/bannerPage';
 import Header from './makanComponents/header';
-
+import About from './makanComponents/about';
 function App() {
   return (
    <div >
     <Header />
     <Banner  />
+    <About />
 
    </div>
   );
---
+++ makan-website/src/makanComponents/about.js
@@ -0,0 +1,23 @@
+
+
+const About = () =>
+{
+    return(
+    <div className = "ml-12 w-1/2 h-12">
+    <b><h1 className = "mt-6 mb-2">About</h1></b>
+     <p>
+     Makan .. where its easy to find Where to go
+     since the first question whenever you hang 
+     out with your friends is "Where to go ?"
+     and the Answer We All Know is "hmm Dont Know"
+     so here you can look up different places With 
+     enough details and choose
+     </p>
+     <br/>
+     <br/>
+     <br/>
+    </div>        
+     
+    )
+} 
+export default About
