d98ad1d3d88a2bdc021521b7e0ba0639ef57bc1a
AHD-02 <a.alziq2002@gmail.com>
2022-04-11 13:53:43 +0300

    updates

--- makan-website/src/makanComponents/header.js
+++ makan-website/src/makanComponents/header.js
@@ -26,7 +26,7 @@ const Header = () =>
         </div>
         <div class="hidden sm:block sm:ml-6">
           <div class="flex space-x-4">
-            <a href="#" class="bg-gray-900 text-white px-1 py-2 rounded-md text-sm font-medium" aria-current="page">Home</a>
+            <a href="#" class="text-gray-300 hover:bg-gray-700 px-1 py-2 rounded-md text-sm font-medium" aria-current="page">Home</a>
 
             <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white px-1 py-2 rounded-md text-sm font-medium">About us</a>
 
@@ -46,17 +46,13 @@ const Header = () =>
         <div class="ml-3 relative">
           <div>
             <button type="button" class="bg-gray-800 flex text-sm rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white" id="user-menu-button" aria-expanded="false" aria-haspopup="true">
-              <span class="sr-only">Open user menu</span>
+              <span class = "sr-only">Open user menu</span>
               <img class="h-8 w-8 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" />
             </button>
           </div>
 
 
-          <div class="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button" tabindex="-1">
-            <a href="#" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1" id="user-menu-item-0">Your Profile</a>
-            <a href="#" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1" id="user-menu-item-1">Settings</a>
-            <a href="#" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1" id="user-menu-item-2">Sign out</a>
-          </div>
+          
         </div>
       </div>
     </div>
