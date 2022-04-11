660a92d0a514ed4eaa115da14788e8f92c95b0ef
AHD-02 <a.alziq2002@gmail.com>
2022-04-11 13:25:14 +0300

    updates

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
 # MakanProject
 # test
-
--- makan-website/src/App.js
+++ makan-website/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import Banner from './makanComponents/bannerPage';
+import Header from './makanComponents/header';
 
 function App() {
   return (
    <div >
+    <Header />
     <Banner  />
 
    </div>
